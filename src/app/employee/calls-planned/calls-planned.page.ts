3221eed563aad2b7f25c2ab0514726aa562ed556
Ercel Peter Sotto <ercelpetersotto@gmail.com>
2019-07-30 14:07:02 +1200

    Fix for the issue where app freezes when marking off an activity from the following lists: Calls - AFSV, Calls - Planned, Meetings - Planned.

--- src/app/employee/calls-planned/calls-planned.page.ts
+++ src/app/employee/calls-planned/calls-planned.page.ts
@@ -79,6 +79,7 @@ export class CallsPlannedPage implements OnInit {
                 OppId: data.result[i].parent_id,
                 OppName: data.result[i].subject,
                 ContactId: data.result[i].contact_id,
+                ActivityId: data.result[i].id,
                 ActivityType: data.result[i].activitytype,
                 StartDate: moment(data.result[i].date_start).format("DD MMM, YYYY") + " " + data.result[i].time_start.substring(0, 5),
                 EventName: data.result[i].cf_985
@@ -173,6 +174,7 @@ export class CallsPlannedPage implements OnInit {
                 OppId: data.result[i].parent_id,
                 OppName: data.result[i].subject,
                 ContactId: data.result[i].contact_id,
+                ActivityId: data.result[i].id,
                 ActivityType: data.result[i].activitytype,
                 StartDate: moment(data.result[i].date_start).format("DD MMM, YYYY") + " " + data.result[i].time_start.substring(0, 5),
                 EventName: data.result[i].cf_985
